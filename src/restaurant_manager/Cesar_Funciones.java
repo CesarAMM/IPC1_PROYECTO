7e9f38b6ca50043a1fff3d213fe9e7dadc959938
Adolfo Mazariegos <csaradolfo139@outlook.com>
2021-06-18 11:10:19 -0600

    Save: carga de datos JSon

--- src/restaurant_manager/Cesar_Funciones.java
+++ src/restaurant_manager/Cesar_Funciones.java
@@ -1,10 +1,12 @@
 package restaurant_manager;
 
-import POO.Restaurante;
+import POO.*;
 import com.google.gson.Gson;
+import java.util.ArrayList;
 
 public class Cesar_Funciones {
-    public static void InitDownloadData(){
+    
+    public static Restaurante InitDownloadData(){
         String paht_json = "C:\\Users\\cesar.monroy\\Desktop\\ipc\\PruebasProyecto\\Json\\";//Para ir a buscar los datos del Json
         String paht_bin = "C:\\Users\\cesar.monroy\\Desktop\\ipc\\PruebasProyecto\\Bin\\";//Para ir a buscar datos Binarios
         //Iniciamos con el config.json
@@ -16,13 +18,76 @@ public class Cesar_Funciones {
         }else if(restaurante.getLoad().equals("bin")){
             CargaDatos_Bin(paht_bin);
         }
+        return restaurante;
     }
     
     private static void CargaDatos_Json(String paht){
-        
+        Gson gson = new Gson();
+        String datos_users = Consola.readFile(paht + "users.json");
+        String datos_clients = Consola.readFile(paht+ "clients.json");
+        String datos_invoices = Consola.readFile(paht + "invoices.json");
+        String datos_products = Consola.readFile(paht+ "products.json");
+        Usuario[] users= gson.fromJson(datos_users, Usuario[].class);
+        Producto[] products = gson.fromJson(datos_products, Producto[].class);
+        Cliente[] clientes = gson.fromJson(datos_clients, Cliente[].class);
+        Factura[] facturas = gson.fromJson(datos_invoices, Factura[].class);
+        boolean estado = false;
+        for (int i = 0; i < users.length; i++) {
+            estado = true;
+            for (Usuario usuario : Restaurant_Manager.usuarios) {
+                if(usuario.getUsername().equals(users[i].getUsername())){
+                    estado = false;
+                    Consola.InsertNewError("USERS", "El id: "+ users[i].getUsername()+ " ya existe, se omitio el registro");
+                    break;
+                }
+            }
+            if(estado == true){
+                Restaurant_Manager.usuarios.add(users[i]);
+            }
+        }
+        for (int i = 0; i < products.length; i++) {
+            estado = true;
+            for (Producto producto : Restaurant_Manager.productos) {
+                if(producto.getId() == products[i].getId()){
+                    estado = false;
+                    Consola.InsertNewError("PRODUCTS", "El id: "+ products[i].getId()+ " ya existe, se omitio el registro");
+                    break;
+                }
+            }
+            if(estado == true){
+                Restaurant_Manager.productos.add(products[i]);
+            }
+        }
+        for (int i = 0; i < clientes.length; i++) {
+            estado = true;
+            for (Cliente cliente : Restaurant_Manager.clientes) {
+                if(cliente.getId() == clientes[i].getId()){
+                    estado = false;
+                    Consola.InsertNewError("CLIENTS", "El id: "+ clientes[i].getId()+ " ya existe, se omitio el registro");
+                    break;
+                }
+            }
+            if(estado == true){
+                Restaurant_Manager.clientes.add(clientes[i]);
+            }
+        }
+        for (int i = 0; i < facturas.length; i++) {
+            estado = true;
+            for (Factura factura : Restaurant_Manager.facturas) {
+                if(factura.getClient()== facturas[i].getClient()){
+                    estado = false;
+                    Consola.InsertNewError("INVOICES", "El id: "+ facturas[i].getClient()+ " ya existe, se omitio el registro");
+                    break;
+                }
+            }
+            if(estado == true){
+                Restaurant_Manager.facturas.add(facturas[i]);
+            }
+        }
     }
     
     private static void CargaDatos_Bin(String paht){
         
     }
+    
 }
--- src/restaurant_manager/Restaurant_Manager.java
+++ src/restaurant_manager/Restaurant_Manager.java
@@ -4,12 +4,34 @@ import POO.*;
 import java.util.ArrayList;
 
 public class Restaurant_Manager {
-    public ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-    public ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-    public ArrayList<Factura> facturas = new ArrayList<Factura>();
-    public ArrayList<Producto> productos = new ArrayList<Producto>();
+    public static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+    public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    public static ArrayList<Factura> facturas = new ArrayList<Factura>();
+    public static ArrayList<Producto> productos = new ArrayList<Producto>();
+    private static String user = "";
+    private static String pass = "";
+    private static Restaurante restaurante;
     public static void main(String[] args) {
-        Cesar_Funciones.InitDownloadData();
+        restaurante = Cesar_Funciones.InitDownloadData();
+        Iniciar();
     }
     
+    public static void Iniciar(){
+        boolean estado = true;
+        do {
+            user = Consola.inputString("Ingrese Usuario: ");
+            pass = Consola.inputString("Ingrese Contraseña: ");
+            for (Usuario usuario : usuarios) {
+                if(usuario.getUsername().equals(user) && usuario.getPassword().equals(pass)){
+                    estado = false;
+                }
+            }
+            if(estado){
+                System.out.println("-->Error de inicio<--");
+                Consola.InsertNewLog(user, "Inicio de sesión faido");
+            }else if(!(user.equals("") && pass.equals(""))){
+                Consola.InsertNewLog(user, "Inicio Seción");
+            }
+        } while (estado);
+    }
 }
